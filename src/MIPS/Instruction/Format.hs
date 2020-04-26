2ee84653db36d84696a781e0aa96aa3b49b4db7b
SchrodingerZhu <i@zhuyi.fan>
2020-04-26 20:52:18 +0800

    add ninja

--- src/MIPS/Instruction/Format.hs
+++ src/MIPS/Instruction/Format.hs
@@ -1,23 +1,41 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE MultiWayIf #-}
 
+{-|
+  Module      : MIPS.Instruction.Format
+  Description : This module defines four types of MIPS binary code format.
+  License     : MIT
+  Maintainer  : i@zhuyi.fan
+-}
 module MIPS.Instruction.Format where
 
 import Clash.Prelude
 
-data Format
-  = NoType
-  | RType
-      (BitVector 6)
-      (BitVector 5)
-      (BitVector 5)
-      (BitVector 5)
-      (BitVector 5)
-      (BitVector 6)
-  | IType (BitVector 6) (BitVector 5) (BitVector 5) (BitVector 16)
-  | JType (BitVector 6) (BitVector 26)
-  deriving (Show)
+-- | The Format of MIPS Instructions
+data Format            
+  = NoType             -- ^ NoType (Specialized for NOP Instruction)
+  | RType              -- ^ R-Type Instruction Format
+      (BitVector 6)    -- ^ Operation Code
+      (BitVector 5)    -- ^ Register S
+      (BitVector 5)    -- ^ Register T
+      (BitVector 5)    -- ^ Register D
+      (BitVector 5)    -- ^ Extra Infomation for Shifting Amount
+      (BitVector 6)    -- ^ Function Code
+  | IType              -- ^ I-Type Instruction Format
+      (BitVector 6)    -- ^ Operation Code
+      (BitVector 5)    -- ^ Register S
+      (BitVector 5)    -- ^ Register T
+      (BitVector 16)   -- ^ Immediate Value
+  | JType              -- ^ J-Type Instruction Format 
+      (BitVector 6)    -- ^ Operation Code
+      (BitVector 26)   -- ^ Jump Target
+  deriving Show
 
+
+{-|
+  Transform raw instruction binary code into instruction format types.
+  This function should be used together with `decodeTyped` to generate the final instruction types.
+-}
 decodeFormat :: BitVector 32 -> Format
 decodeFormat 0 = NoType
 decodeFormat vec =
